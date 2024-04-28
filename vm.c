b5f07943cee046d4133b84ba78dc07dc8c7e7d44
Adam Kelly <adammkelly@live.com>
2024-04-28 14:43:19 +0100

    Chapter 15 Section 3

--- vm.c
+++ vm.c
@@ -39,6 +39,13 @@ run()
 {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op) \
+    do { \
+      double b = pop(); \
+      double a = pop(); \
+      push(a op b); \
+    } while (false)
+
 
     for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
@@ -60,6 +67,21 @@ run()
                 push(constant);
                 break;
             }
+            case OP_ADD:
+                BINARY_OP(+);
+                break;
+            case OP_SUBTRACT:
+                BINARY_OP(-);
+                break;
+            case OP_MULTIPLY:
+                BINARY_OP(*);
+                break;
+            case OP_DIVIDE:
+                BINARY_OP(/);
+                break;
+            case OP_NEGATE:
+                push(-pop());
+                break;
             case OP_RETURN: {
                 print_value(pop());
                 printf("\n");
@@ -69,6 +91,7 @@ run()
     }
 #undef READ_BYTE
 #undef READ_CONSTANT
+#undef BINARY_OP
 }
 
 interpret_result_t
--- vm.h
+++ vm.h
@@ -21,7 +21,7 @@ typedef enum {
 
 void init_vm();
 void free_vm();
-interpret_result_t intercept(chunk_t *chunk);
+interpret_result_t interpret(chunk_t *chunk);
 void push(value_t value);
 value_t pop();
 
